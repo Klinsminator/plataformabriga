10e64f8b0861d1095abe941c482db16891eca2ce
Jurgen Azofeifa Flores <jurgenazofeifa@gmail.com>
2020-06-20 19:35:46 -0600

    Adding files for login view

---
+++ resources/views/includes/head_Login_v1.blade.php
@@ -0,0 +1,20 @@
+<head>
+    <title>P | Abriga</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="{{asset ('images/icons/favicon.ico')}}"/>
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset ('css/bootstrap.min.css')}}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset ('fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset ('css/Login_v1/animate.css')}}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset ('css/Login_v1/hamburgers.min.css')}}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset ('css/Login_v1/select2.min.css')}}">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{asset ('css/app.css')}}">
+    <!--===============================================================================================-->
+</head>
